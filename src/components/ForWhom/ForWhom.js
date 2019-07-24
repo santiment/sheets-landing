71fae1b987f40d5874cb0991604bfaa584b44b8a
haritonasty <a.kharitonovaspb@gmail.com>
2019-07-24 22:35:52 +0300

    rewrite use cases

--- src/components/ForWhom/ForWhom.js
+++ src/components/ForWhom/ForWhom.js
@@ -1,24 +1,26 @@
 import React from "react"
+import Title from '../Title/Title'
+import Subtitle from '../Subtitle/Subtitle'
 import styles from './ForWhom.module.scss'
 
 const ForWhom = () => {
   return (
     <section>
       <article className={styles.wrapper}>
-        <h2 className={styles.title}>For traders & investors</h2>
-        <p className={styles.description}>
+        <Title small className={styles.title}>For traders & investors</Title>
+        <Subtitle className={styles.description}>
           Build models and backtest your strategies with the most comprehensive
           source of crypto data on the market. No data aggregation, no
           intermediaries.
-        </p>
+        </Subtitle>
       </article>
       <article className={styles.wrapper}>
-        <h2 className={styles.title}>For researches & journalists</h2>
-        <p className={styles.description}>
+        <Title small className={styles.title}>For researches & journalists</Title>
+        <Subtitle className={styles.description}>
           Investigate network activity, discover market patterns, and analyze
           stakeholder behavior with our clean, reliable data streams for 100s of
           digital assets.
-        </p>
+        </Subtitle>
       </article>
     </section>
   )
