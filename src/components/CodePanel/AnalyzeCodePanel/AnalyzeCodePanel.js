963a4fb64896080f1939743ebfe5d337fba68561
haritonasty <a.kharitonovaspb@gmail.com>
2019-07-25 02:45:28 +0300

    finish analyze code panel

--- src/components/CodePanel/AnalyzeCodePanel/AnalyzeCodePanel.js
+++ src/components/CodePanel/AnalyzeCodePanel/AnalyzeCodePanel.js
@@ -5,6 +5,7 @@ import social from '../FloatingPanel/social.svg'
 import devact from '../FloatingPanel/devact.svg'
 import financial from '../FloatingPanel/financial.svg'
 import onchain from '../FloatingPanel/onchain.svg'
+import eth from '../FloatingPanel/eth.png'
 import styles from "./AnalyzeCodePanel.module.scss"
 
 const AnalyzeCodePanel = () => (
@@ -14,6 +15,17 @@ const AnalyzeCodePanel = () => (
       <FloatingPanel className={styles.onchain} title="On-chain" icon={onchain} />
       <FloatingPanel className={styles.social} title="Social" icon={social} />
       <FloatingPanel className={styles.devact} title="Dev. activity" icon={devact} />
+      <div className={styles.ethereum}>
+          <div className={styles.title}>
+          <img src={eth} alt="eth logo" />
+          <h3 className={styles.titleText}>Ethereum</h3>
+          </div>
+          <h4 className={styles.description}>
+            Ethereum price was decreased by
+            <bold className={styles.bold}>$ 70.51</bold>
+             in 24 hours
+          </h4>
+        </div>
     </div>
   </CodePanel>
 )
