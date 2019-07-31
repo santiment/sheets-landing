9578e989e93aed529e1bb1ab222af724d937e09b
haritonasty <a.kharitonovaspb@gmail.com>
2019-07-31 09:56:15 +0300

    added functions

--- src/components/CodePanel/MainCodePanel/Suggestions.js
+++ src/components/CodePanel/MainCodePanel/Suggestions.js
@@ -2,29 +2,39 @@ import React from "react"
 import styles from './Suggestions.module.scss'
 
 const functions = [
+  "SAN_NETWORK_GROWTH",
+  "SAN_PRICES",
+  "SAN_PRICE_PERCENT_CHANGE",
+  "SAN_PRICE_ABSOLUTE_CHANGE",
   "SAN_ALL_PROJECTS",
   "SAN_ERC20_PROJECTS",
-  "SAN_PROJECT_FUNDAMENTALS",
-  "SAN_OHLC",
-  "SAN_LATEST_PRICE",
-  "SAN_PRICES",
-  "SAN_DAILY_CLOSING_PRICE",
-  "SAN_PRICE_PERCENT_CHANGE",
-  "SAN_SOCIAL_VOLUME",
   "SAN_ACTIVE_ADDRESSES",
-  "SAN_ACTIVE_DEPOSITS",
-  "SAN_PRICE_VOLUME_DIFF",
   "SAN_TRANSACTION_VOLUME",
-  "SAN_NETWORK_GROWTH",
+  "SAN_OHLC",
+  "SAN_PRICE_VOLUME_DIFF",
+  "SAN_SOCIAL_VOLUME",
+  "SAN_SOCIAL_VOLUME_PROJECTS",
+  "SAN_GITHUB_ACTIVITY",
   "SAN_DEV_ACTIVITY",
-  "SAN_REALIZED_VALUE",
-  "SAN_MVRV_RATIO",
-  "SAN_TOKEN_AGE_CONSUMED",
   "SAN_EXCHANGE_FUNDS_FLOW",
   "SAN_TOKEN_CIRCULATION",
+  "SAN_TRENDING_WORDS",
+  "SAN_PROJECT_FUNDAMENTALS",
+  "SAN_PROJECT_SOCIAL_DATA",
+  "SAN_TOKEN_AGE_CONSUMED",
+  "SAN_MVRV_RATIO",
   "SAN_NVT_RATIO",
-  "SAN_MINING_POOLS_DISTRIBUTION",
+  "SAN_ACTIVE_DEPOSITS",
+  "SAN_REALIZED_VALUE",
   "SAN_GAS_USED",
+  "SAN_MINING_POOLS_DISTRIBUTION",
+  "SAN_MINERS_BALANCE",
+  "SAN_NEWS",
+  "SAN_LATEST_PRICE",
+  "SAN_DAILY_CLOSING_PRICE",
+  "SAN_HISTORICAL_BALANCE",
+  "SAN_HISTORY_TWITTER_DATA",
+  "SAN_SOCIAL_DOMINANCE",
 ]
 
 const Suggestions = ({ currentValue }) => {
@@ -32,8 +42,18 @@ const Suggestions = ({ currentValue }) => {
   return (
     <div className={styles.suggestions}>
       {(currentValue ? filteredSuggestions : functions)
-        .map(suggest => (
-          <div key={suggest} className={styles.suggestion}>{suggest}</div>
+        .map((suggest,i) => (
+          <div
+            key={i}
+            className={styles.suggestion}
+            >
+          <a
+                target='_blank'
+                rel='noopener noreferrer'
+                children={suggest}
+                href={`https://github.com/santiment/google-spreadsheet-addon/blob/master/doc/sheet_functions.md#${suggest}`}
+             />
+          </div>
           ))}
         {currentValue && filteredSuggestions.length === 0 && <div className={styles.empty}>No results found</div>}
     </div>
