d034de163ea7f82040e6f8d6a9fd68adc664d5bd
haritonasty <a.kharitonovaspb@gmail.com>
2020-03-12 18:25:54 +0300

    first promoter

--- src/html.js
+++ src/html.js
@@ -21,6 +21,15 @@ const gtagScript = (
   />
 )
 
+const firstPromoterScript = (
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `  (function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src='https://cdn.firstpromoter.com/fprom.js',t.onload=t.onreadystatechange=function(){var t=this.readyState;if(!t||"complete"==t||"loaded"==t)try{$FPROM.init("ks4svmdl",".santiment.net")}catch(t){}};var e=document.getElementsByTagName("script")[0];
+            e.parentNode.insertBefore(t,e)})();`
+    }}
+  />
+)
+
 export default function HTML (props) {
   return (
     <html {...props.htmlAttributes}>
@@ -51,6 +60,7 @@ export default function HTML (props) {
           dangerouslySetInnerHTML={{ __html: props.body }}
         />
         {props.postBodyComponents}
+        {firstPromoterScript}
       </body>
     </html>
   )
