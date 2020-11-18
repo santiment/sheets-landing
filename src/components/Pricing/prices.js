e7174d2f7d1f5e233a9c3b95f3da80943c625414
DmitriVanGuard <dimavanguard@gmail.com>
2020-11-18 18:56:30 +0300

    fix

--- src/components/Pricing/prices.js
+++ src/components/Pricing/prices.js
@@ -3,19 +3,21 @@ import Button from '@santiment-network/ui/Button'
 import PaymentDialog from '../PaymentDialog/PaymentDialog'
 import ChangePlanDialog from '../ChangePlanDialog/ChangePlanDialog'
 import PipedriveDialogBtn from '../Pipedrive/Pipedrive'
-import { isJapanese } from "../Footer/Footer"
+import { isJapanese } from '../Footer/Footer'
 import { tr } from '../../utils/translate'
 import styles from './index.module.scss'
 
-const PlanActionDialog = props =>
+const PlanActionDialog = (props) =>
   props.subscription ? (
     <ChangePlanDialog {...props} />
   ) : (
     <PaymentDialog {...props} />
   )
 
-const EN_FORM_LINK = 'https://pipedrivewebforms.com/form/90507f6b3cfe0f8218895f8e0384945b4144829'
-const JP_FORM_LINK = 'https://pipedrivewebforms.com/form/ac4b6fd0208f80e916822b0b5ac7ebca4144829'
+const EN_FORM_LINK =
+  'https://pipedrivewebforms.com/form/90507f6b3cfe0f8218895f8e0384945b4144829'
+const JP_FORM_LINK =
+  'https://pipedrivewebforms.com/form/ac4b6fd0208f80e916822b0b5ac7ebca4144829'
 
 export default {
   FREE: {
@@ -63,19 +65,33 @@ export default {
       </span>,
     ],
   },
+  PRO_PLUS: {
+    Component: PlanActionDialog,
+    title: 'Pro+',
+    link: 'Start Free 14-Day Trial',
+    features: [
+      <b>All in PRO and:</b>,
+      'Dedicated account manager',
+      'Basic API 300k API calls',
+      'Closed Webinars with Santiment Analytics',
+      'Closed chat with Santiment market analysts',
+      'Custom education & onboarding',
+    ],
+  },
   ENTERPRISE: {
     discount: 'price.bill_discount.custom',
     link: 'cta.contact',
-    Component: props => {
+    Component: (props) => {
       const isJp = isJapanese()
 
-      return <PipedriveDialogBtn
-        {...props}
-        title='Enterprise plan Pipedrive form'
-        src={isJp ? JP_FORM_LINK : EN_FORM_LINK }
-      />
-    }
-    ,
+      return (
+        <PipedriveDialogBtn
+          {...props}
+          title='Enterprise plan Pipedrive form'
+          src={isJp ? JP_FORM_LINK : EN_FORM_LINK}
+        />
+      )
+    },
     features: [
       'Timeseries metrics - 3 years of historical data',
       'Timeseries metrics - including present-day data',
